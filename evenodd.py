96bbe72f4f3e05f97488c79ca66dafc14cb09086
athu1512 <athulya1512@gmail.com>
2020-04-30 20:12:00 +0530

    task2

---
+++ evenodd.py
@@ -0,0 +1,5 @@
+def evenodd(n):
+    if(n%2==0):
+        print("It is even")
+    else:
+        print("It is odd")
---
+++ input1.py
@@ -0,0 +1,3 @@
+import evenodd as e
+n=int(input("enter the number:"))
+e.evenodd(n)
